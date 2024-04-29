472ceb8a0ee17460e6fb4f591dcedd119968028b
David Kalliecharan <david@definedata.net>
2024-04-28 22:34:03 -0300

    Added unit test with path testing

--- src/main.rs
+++ src/main.rs
@@ -29,63 +29,12 @@
 //! * Send newsletter to subscribers
 //! * Allow authors to send emails to subscribers
 
-use axum::{
-    extract::Path, http::StatusCode, response::IntoResponse, routing::get,
-    Router,
-};
 use clap::Parser;
-
-#[derive(Parser)]
-struct Cli {
-    /// ip address
-    #[clap(short, long, default_value = "127.0.0.1")]
-    addr: String,
-    /// ip port
-    #[clap(short, long, default_value_t = 9000)]
-    port: u16,
-}
-
-/// Greet the listner
-///
-/// # Parameters
-/// - `name`: Name to be greeted as
-///
-/// # Returns
-/// A String greeting
-async fn greet(Path(name): Path<String>) -> String {
-    format!("Hello {}!\n", name)
-}
-
-/// Health check
-///
-/// Alive server check for external services.
-///
-/// # Returns
-/// `(SuccessCode::OK, "")`
-async fn health_check() -> impl IntoResponse {
-    (StatusCode::OK, "")
-}
+use std::io::Result;
+use zero2prod_axum::{run, Cli};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let cli = Cli::parse();
-    let addr = cli.addr;
-    let port = cli.port.to_string();
-    // Naive way to create a binded address
-    let bind_addr = format!("{}:{}", addr, port);
-
-    // Define single routes for now
-    let app = Router::new()
-        .route(
-            "/",
-            get(|| async {
-                "Welcome to an Axum Zero to Production implementation!\n"
-            }),
-        )
-        .route("/:name", get(greet))
-        .route("/health_check", get(health_check));
-
-    // Run app using hyper while listening onto the configured port
-    let listener = tokio::net::TcpListener::bind(bind_addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    run(cli).await
 }
