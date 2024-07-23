30363fc9a4e4d9134da68d8c7bc7ff1bb63d72d0
David Kalliecharan <david@definedata.net>
2024-07-22 21:55:01 -0300

    Add FALSE sense of safety

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -22,6 +22,7 @@ use axum::{http::StatusCode, response::IntoResponse, Extension, Form};
 use chrono::{DateTime, Utc};
 use serde::Deserialize;
 use sqlx::SqlitePool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -47,10 +48,31 @@ pub async fn subscriptions(
     Extension(pool): Extension<SqlitePool>,
     Form(sign_up): Form<SignUp>,
 ) -> impl IntoResponse {
+    if !is_valid_name(&sign_up.name) {
+        return StatusCode::BAD_REQUEST;
+    }
     match insert_subscriber(Extension(pool), Form(sign_up)).await {
         Ok(_) => StatusCode::OK,
         Err(_) => StatusCode::SERVICE_UNAVAILABLE,
     }
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    // Grapheme is defined by the unicode standard as a "user-percieved"
+    // character: `ö` is a single grapheme, but composed of two
+    // characeters o(`o` and `¨`)
+    //
+    // `.grapheme(true)` returns an iterator that uses the extended
+    // grapheme definition set, the recommended one
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters =
+        s.chars().any(|g| forbidden_characters.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
 
 #[tracing::instrument(
